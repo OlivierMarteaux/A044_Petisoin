18273c70b23608223df08baa3a75039136e798f6
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-05-19 17:52:41 +0200

    00_20250519_OM: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# A044_Petisoin  
+<!-- This is a comment --> 
+  
+<!-- ![Screen 1](assets/images/screen01.png)	![Screen 2](assets/images/screen02.png) ![Screen 3](assets/images/screen03.png)  -->
+  
+**OpenClassroom training:**  
+Projet 7 - Cours - Gérez vos données localement pour une application Android 100 % hors-ligne  
+  
+[Link to the kotlin starter repository](https://github.com/OpenClassrooms-Student-Center/8396086-Gerez-donnees-localement-application-hors-ligne-Kotlin/tree/main)  
+
+[Link to the java starter repository](https://github.com/OpenClassrooms-Student-Center/8396086-Gerez-donnees-localement-application-hors-ligne-java)  
+
+[Link to the Openclassroom project](https://openclassrooms.com/fr/courses/8396086-gerez-vos-donnees-localement-pour-une-application-android-100-hors-ligne)   
+  <!--
+[Link to the screen recorder](assets/screenRecorder/)  
+  
+[Link to the UI tests report](assets/tests/)  
+  
+  
+### Objectives  
+  
+- [x] Add authentication system with Api login request on login screen
+- [x] Fetch user account data from Api on home screen
+- [x] Add account transfer system with Api transfer request on transfer screen
+- [x] For all screens take into account loading, success and error paths
+- [x] Perform all unit local tests
+--> 
---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
+}
+
+android {
+    namespace = "com.example.a044_petisoin"
+    compileSdk = 35
+
+
+    room {
+        // Enables exporting database schemas into JSON files in the given directory.
+        schemaDirectory("$projectDir/schemas")
+    }
+
+    defaultConfig {
+        applicationId = "com.example.a044_petisoin"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        ksp.apply {
+            // Enables exporting database schemas into JSON files in the given directory.
+//            arg("room.schemaLocation", "$projectDir/schemas")
+            // Enables Gradle incremental annotation processor. Default value is true.
+            arg("room.incremental", "true")
+            // Generate Kotlin source files instead of Java. Requires KSP. Default value is true as of version 2.7.0.
+            arg("room.generateKotlin", "true")
+        }
+
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    // ROOM
+    implementation(libs.androidx.room.runtime)
+    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+    ksp(libs.androidx.room.compiler)
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    // annotationProcessor(libs.androidx.room.compiler)
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation(libs.androidx.room.ktx)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
